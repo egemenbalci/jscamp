9690703e6b6b63f39f7ecf979e10724d6d392781
egemenbalci <64485663+egemenbalci@users.noreply.github.com>
2021-10-29 19:01:13 +0300

    29 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun= 9.20
+let dolarBugun=9.30
+
+console.log(dolarBugun)
+
+const euroDun= 11.2
+//euroDun=11
+console.log(euroDun)
+
+//array
+
+let konutKredileri= ["Konut kredisi", "Emlak Konut Kredisi","Özel Konut Kredisi", "Kamu Konut Kredisi"]
+
+console.log("<ul>")
+
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
+
+console.log(konutKredileri)
+
